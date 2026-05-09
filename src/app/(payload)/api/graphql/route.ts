112daead249e1b237572ec57babb393c5fedbb2c
tolgaarabogludev <tolgaarabogludev@gmail.com>
2026-05-09 13:11:25 +0300

    Fix graphql route config import

--- src/app/(payload)/api/graphql/route.ts
+++ src/app/(payload)/api/graphql/route.ts
@@ -1,4 +1,4 @@
-import configPromise from "@payload-config"
+import config from "@payload-config";
 import { GRAPHQL_POST, REST_OPTIONS } from "@payloadcms/next/routes";
 
 export const POST = GRAPHQL_POST(config);
