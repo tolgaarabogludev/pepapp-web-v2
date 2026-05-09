b3c3492f733684e717cc838360a777f85472287b
tolgaarabogludev <tolgaarabogludev@gmail.com>
2026-05-09 13:06:22 +0300

    Fix graphql playground config import

--- src/app/(payload)/api/graphql-playground/route.ts
+++ src/app/(payload)/api/graphql-playground/route.ts
@@ -1,4 +1,4 @@
-import configPromise from "@payload-config"
+import config from "@payload-config";
 import { GRAPHQL_PLAYGROUND_GET } from "@payloadcms/next/routes";
 
 export const GET = GRAPHQL_PLAYGROUND_GET(config);
